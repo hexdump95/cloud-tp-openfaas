4193e5e3f5f78fd1d76eed118844098ec7bd32f8
Sergio Villanueva <sergiovillanueva@protonmail.com>
2021-11-04 17:16:56 -0300

    Add jsonpath dependency

--- image/src/main/java/com/openfaas/function/Handler.java
+++ image/src/main/java/com/openfaas/function/Handler.java
@@ -5,12 +5,31 @@ import com.openfaas.model.IResponse;
 import com.openfaas.model.IRequest;
 import com.openfaas.model.Response;
 
+import static com.jayway.jsonpath.JsonPath.read;
+
 public class Handler extends com.openfaas.model.AbstractHandler {
 
     public IResponse Handle(IRequest req) {
-        Response res = new Response();
-	    res.setBody("Hello, world!");
+        String bucket = new String();
+        String key = new String();
 
-	    return res;
+        Response res = new Response();
+
+        try {
+            bucket = read(req.getBody(), "$.Records[0].s3.bucket.name");
+            key = read(req.getBody(), "$.Records[0].s3.object.key");
+        } catch (Exception e) {
+            res.setHeader("Content-Type", "application/json");
+            res.setStatusCode(400);
+            res.setBody("{\"message\": \"Body should be a file\"}");
+            return res;
+        }
+
+        System.out.println(bucket);
+        System.out.println(key);
+
+        res.setBody("Hello, world!");
+
+        return res;
     }
 }
