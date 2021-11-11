0f0e7c02aa7e493d30b46ae353b13323785eb2d3
Sergio Villanueva <sergiovillanueva@protonmail.com>
2021-11-11 16:47:50 -0300

    Add created_at in mongo collection

--- openfaas/image/src/main/java/com/openfaas/function/Handler.java
+++ openfaas/image/src/main/java/com/openfaas/function/Handler.java
@@ -2,7 +2,6 @@ package com.openfaas.function;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Files;
-import com.mongodb.MongoException;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
@@ -13,41 +12,48 @@ import com.openfaas.model.IResponse;
 import com.openfaas.model.IRequest;
 import com.openfaas.model.Response;
 import org.bson.Document;
-import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static com.jayway.jsonpath.JsonPath.read;
 
 public class Handler extends AbstractHandler {
+    public static Logger log = LoggerFactory.getLogger(Handler.class);
 
     public IResponse Handle(IRequest req) {
-        String bucket;
-        String key;
         String uri = "";
 
         Response res = new Response();
         try {
             uri = Files.asCharSource(new File("/var/openfaas/secrets/secret-mongodb-uri"), Charsets.UTF_8).readFirstLine();
         } catch (IOException e) {
-            res.setStatusCode(400);
+            log.error(e.getMessage());
         }
 
         try (MongoClient mongoClient = MongoClients.create(uri)) {
             MongoDatabase database = mongoClient.getDatabase("imageDB");
             MongoCollection<Document> collection = database.getCollection("images");
             try {
-                bucket = read(req.getBody(), "$.Records[0].s3.bucket.name");
-                key = read(req.getBody(), "$.Records[0].s3.object.key");
+                String bucket = read(req.getBody(), "$.Records[0].s3.bucket.name");
+                String key = read(req.getBody(), "$.Records[0].s3.object.key");
+                String createdAt = Instant.parse(read(req.getBody(), "$.Records[0].eventTime"))
+                        .atZone(ZoneId.of("America/Argentina/Buenos_Aires"))
+                        .format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ"));
 
-                collection.insertOne(new Document()
+                InsertOneResult insertOne = collection.insertOne(new Document()
                         .append("bucket", bucket)
-                        .append("key", key));
-
-                res.setStatusCode(201);
+                        .append("key", key)
+                        .append("created_at", createdAt));
+                log.info(insertOne.toString());
             } catch (Exception e) {
-                res.setStatusCode(400);
+                log.error(e.getMessage());
             }
         }
         return res;
